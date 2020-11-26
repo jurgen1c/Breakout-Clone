84954820baaaa88a47b7df1ab81c112d448017e3
jurgen1c <jurgen1c@gmail.com>
2020-11-26 09:40:52 -0600

    Add brick positioning & collision

--- Assets/Js/app.js
+++ Assets/Js/app.js
@@ -51,7 +51,7 @@ var bricks = [];
 for(var c = 0; c < brickColumnCount; c++){
   bricks[c] = [];
   for(var r = 0; r < brickRowCount; r++){
-    bricks[c][r] = {x: 0, y: 0};
+    bricks[c][r] = {x: 0, y: 0, status: 1};
   }
 }
 
@@ -74,6 +74,17 @@ function keyUpHandler(e){
   }
 }
 
+function collisionDetection(){
+  for(var c = 0; c < brickColumnCount; c++){
+    for(var r = 0; r < brickRowCount; r++){
+      var b = bricks[c][r];
+      if(x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight){
+        dy = -dy;
+      }
+    }
+  }
+}
+
 function drawBall(){
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -93,19 +104,24 @@ function drawPaddle(){
 function drawBricks(){
   for(var c = 0; c < brickColumnCount; c++){
     for(var r = 0; r < brickRowCount; r++){
-      bricks[c][r].x = 0;
-      bricks[c][r].y = 0;
-      ctx.beginPath();
-      ctx.rect(0, 0, brickWidth, brickHeight);
-      ctx.fillStyle = "#0095DD";
-      ctx.fill();
-      ctx.closePath();
+      if(bricks[c][r].status == 1){
+        var brickX = (c*(brickWidth+brickPadding)) + brickOffsetLeft;
+        var brickY = (r*(brickHeight+brickPadding)) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
     }
   }
 }
 
 function draw(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBricks();
   drawBall();
   drawPaddle();
   
