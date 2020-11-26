efbe7277112de832aa3f16e26fb93a88e1375a28
jurgen1c <jurgen1c@gmail.com>
2020-11-26 07:27:31 -0600

    Add paddle

--- Assets/Js/app.js
+++ Assets/Js/app.js
@@ -1,10 +1,3 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-var x = canvas.width / 2;
-var y = canvas.height - 30;
-var dx = 2;
-var dy = -2;
-var ballRadius = 10;
 /* ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
 ctx.fillStyle = "#FF0000";
@@ -23,6 +16,23 @@ ctx.strokeStyle = "rgb(0, 0, 255, 0.5)";
 ctx.stroke();
 ctx.closePath(); */
 
+// Canvas Bal Vars----------------
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+var ballRadius = 10;
+
+// Paddle Vars--------------
+
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
+
+
 function drawBall(){
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -31,9 +41,18 @@ function drawBall(){
   ctx.closePath();
 }
 
+function drawPaddle(){
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
 function draw(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
   x += dx;
   y += dy;
   if (y + dy < ballRadius || y + dy > canvas.height-ballRadius){
