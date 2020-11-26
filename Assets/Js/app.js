c5df71ca926150d683e470d85983ffef5d19dfa8
jurgen1c <jurgen1c@gmail.com>
2020-11-26 07:21:21 -0600

    Add canvas collison

--- Assets/Js/app.js
+++ Assets/Js/app.js
@@ -1,9 +1,10 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var x = canvas.width / 2;
-var y = canvas.height / 2;
+var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
+var ballRadius = 10;
 /* ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
 ctx.fillStyle = "#FF0000";
@@ -22,9 +23,9 @@ ctx.strokeStyle = "rgb(0, 0, 255, 0.5)";
 ctx.stroke();
 ctx.closePath(); */
 
-function drawBall();{
+function drawBall(){
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI*2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
@@ -35,5 +36,11 @@ function draw(){
   drawBall();
   x += dx;
   y += dy;
+  if (y + dy < ballRadius || y + dy > canvas.height-ballRadius){
+    dy = -dy;
+  } 
+  if(x + dx < ballRadius || x + dx > canvas.width-ballRadius){
+    dx = -dx;
+  }
 }
-setInterval(draw, 10)
+setInterval(draw, 10);
