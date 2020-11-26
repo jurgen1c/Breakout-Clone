8fee0dae453ef7b17f3dc09987c3e1fa6398946e
jurgen1c <jurgen1c@gmail.com>
2020-11-26 09:03:29 -0600

    Add brick vars & and creation logic

--- Assets/Js/app.js
+++ Assets/Js/app.js
@@ -32,10 +32,28 @@ var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width - paddleWidth) / 2;
 
-//Control Vars------------------
+// Control Vars------------------
 
 var rightPressed = false;
 var leftPressed = false;
+
+// Brick Vars--------------------
+
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+var bricks = [];
+
+for(var c = 0; c < brickColumnCount; c++){
+  bricks[c] = [];
+  for(var r = 0; r < brickRowCount; r++){
+    bricks[c][r] = {x: 0, y: 0};
+  }
+}
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -70,6 +88,20 @@ function drawPaddle(){
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
+}
+
+function drawBricks(){
+  for(var c = 0; c < brickColumnCount; c++){
+    for(var r = 0; r < brickRowCount; r++){
+      bricks[c][r].x = 0;
+      bricks[c][r].y = 0;
+      ctx.beginPath();
+      ctx.rect(0, 0, brickWidth, brickHeight);
+      ctx.fillStyle = "#0095DD";
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
 }
 
 function draw(){
